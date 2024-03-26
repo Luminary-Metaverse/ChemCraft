f32db41748729bd12baca94ada2eeda7afec9908
Muhammad Zuhair Zuhdi <joe@frostfire.id>
2024-03-26 10:15:28 +0700

    Initial Commit

---
+++ android/src/id/luminary/chemcraft/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package id.luminary.chemcraft;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import id.luminary.chemcraft.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
